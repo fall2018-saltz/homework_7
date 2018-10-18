65a7b8c43fada87fc9e08f1e67594024fe674fe2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 21:27:10 -0400

    Saving area_map.R

--- area_map.R
+++ area_map.R
@@ -1,6 +1,7 @@
 
 combined_df$stateName <-tolower(combined_df$stateName)
 US <-map_data("state")
+
 US_data_map <- ggplot(combined_df, aes(map_id = stateName))
 US_data_map <- US_data_map + geom_map(map = US, aes(fill = StateArea))
 US_data_map <- US_data_map + expand_limits(x = US$long, y = US$lat)
