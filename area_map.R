99e8a214195bf15f4f2d2942db8efc65933bf5a8
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 11:12:39 -0400

    deleting area_map.R

--- area_map.R
+++
@@ -1,9 +0,0 @@
-
-combined_df$stateName <-tolower(combined_df$stateName)
-US <-map_data("state")
-
-US_data_map <- ggplot(combined_df, aes(map_id = stateName))
-US_data_map <- US_data_map + geom_map(map = US, aes(fill = StateArea))
-US_data_map <- US_data_map + expand_limits(x = US$long, y = US$lat)
-US_data_map <- US_data_map + coord_map() + ggtitle("State Area")
-US_data_map
