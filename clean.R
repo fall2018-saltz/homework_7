c6597ff726d80ff19ed6ebb61e9239c05f5382fa
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 21:55:45 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,9 +1,7 @@
 
-library(ggmap)
-library(ggplot2)
+library (dplyr)
+library (ggplot2)
 library(tibble)
-
-##Load and Merge Datasets
 dfStates <- pop_df
 arrests <- USArrests 
 stateName <- rownames(arrests)
@@ -11,12 +9,11 @@ rownames(arrests) <- NULL
 arrests <- cbind(arrests,stateName)
 arrests
 
-
 ##CLEANING DATA
 dfStates <- dfStates[-53,]
 dfStates <- dfStates[-1,]
 dfStates <- dfStates[,-1:-4]
-View(dfStates)
+dfStates
 ##Making sure there are exactly 51 rows
 nrow(dfStates)
 filter_df <- dfStates
@@ -29,23 +26,9 @@ cnames[4] <- "percentOver18"
 colnames(dfStates) <- cnames
 colnames(dfStates)
 
+##Step B: explore data, understanding distributions
 ##arrests <- arrests %>% rownames_to_column("stateName")
 common_col_names <- intersect(names(arrests), names(dfStates))
 combined_df <- merge.data.frame(arrests, dfStates, by = common_col_names, all.x = TRUE)
-View(combined_df)
-
-##adding state area and state centers to data frame
-statearea <- state.area
-combined_df$StateArea <- statearea
-statecenter <- state.center
-combined_df$StateCenter <- statecenter
-View(combined_df)
-
-##adding area and adding center of each state (alternate, trying to establish X/Y coordinates within dataframe)
-stateStats <- data.frame(state.name, state.center, state.area)
-View(combined_df)
-
-##lowercase
-combined_df$stateName <-tolower(combined_df$stateName)
-US <-map_data("state")
+combined_df
 
